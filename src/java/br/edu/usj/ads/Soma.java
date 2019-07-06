cfea231db69be347a888a3f0832b0f2470f4ae19
Vinicius Ventura Thiesen <viniciusventura46@gmail.com>
2019-07-06 20:55:07 -0300

    commit inicial

---
+++ src/java/br/edu/usj/ads/Soma.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.usj.ads;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author vinicius
+ */
+@WebServlet(name = "Soma", urlPatterns = {"/Soma"})
+public class Soma extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        int a = Integer.parseInt(request.getParameter("a"));
+        int b = Integer.parseInt(request.getParameter("b"));
+        int resultado = a +b;
+        
+        PrintWriter output = response.getWriter();
+            output.println("<html>");
+            output.println("<head>");
+            output.println("<title>Servlet Soma</title>");            
+            output.println("</head>");
+            output.println("<body>");
+            output.println("<p>");
+            output.println("    "+ resultado);
+            output.println("</p>");
+            output.println("</body>");
+            output.println("</html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/java/br/edu/usj/ads/Subtracao.java
@@ -0,0 +1,58 @@
+package br.edu.usj.ads;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author vinicius
+ */
+@WebServlet(name = "Subtracao", urlPatterns = {"/Subtracao"})
+public class Subtracao extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+    }
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        int a = Integer.parseInt(request.getParameter("a"));
+        int b = Integer.parseInt(request.getParameter("b"));
+        int resultado = a - b;
+        
+        PrintWriter output = response.getWriter();
+            output.println("<html>");
+            output.println("<head>");
+            output.println("<title>Servlet Soma</title>");            
+            output.println("</head>");
+            output.println("<body>");
+            output.println("<p>");
+            output.println("    "+ resultado);
+            output.println("</p>");
+            output.println("</body>");
+            output.println("</html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
